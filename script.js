fda7ca559661e248f4aa24b654f25c002f9415ca
PRahul-25 <kitk5928@gmail.com>
2024-07-10 16:04:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var on_button=document.getElementById("on");
+var off_button=document.getElementById("off");
+var image=document.getElementById("bulb");
+
+function bulb_on(){
+    image.src="./image/images_2.jpg";
+}
+function bulb_off(){
+    image.src="./image/images_1.jpg";
+}
